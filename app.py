9603d5f30e9184702f6261a989efcbd8392630a1
VoxAyto <barcog82@gmail.com>
2025-07-08 10:09:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def index():
     resultado = request.args.get('resultado', '')
     fecha = request.args.get('fecha', '')
     concejal = request.args.get('concejal', '')
+se_ha_buscado = any([grupo, palabra_clave, resultado, fecha, concejal])
 
     query = "SELECT fecha, grupo, contenido, resultado, concejal FROM iniciativas WHERE 1=1"
     params = []
@@ -39,7 +40,8 @@ def index():
         query += " AND concejal = ?"
         params.append(concejal)
 
-    resultados_query = conn.execute(query, params).fetchall()
+    resultados_query = conn.execute(query, params).fetchall() if se_ha_buscado else []
+
     return render_template('index.html', resultados=resultados_query, grupos=grupos,
                            resultados_filtro=resultados, fechas=fechas, grupo=grupo,
                            palabra_clave=palabra_clave, resultado=resultado, fecha=fecha,
